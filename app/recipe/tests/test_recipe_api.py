02ecde22ac8214cf1508a57b24fa8e8ad8c7160f
Esharat Mia <esharat1992@gmail.com>
2024-10-10 23:38:48 +0600

    added recipe views, serilaizer, urls

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -8,7 +8,7 @@ from django.test import TestCase
 from django.urls import reverse
 
 from rest_framework import status
-from django_restframework.test import APIClient
+from rest_framework.test import APIClient
 
 from core.models import Recipe
 from recipe.serializers import RecipeSerializer
