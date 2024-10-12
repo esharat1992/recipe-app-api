8a161e9df759391984d36b073cd29bc7e4fefba3
Esharat Mia <esharat1992@gmail.com>
2024-10-13 00:05:45 +0600

    fixed linting error.

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -108,7 +108,6 @@ class PrivateRecipeApiTests(TestCase):
             'title': 'Sample recipe',
             'require_time': 10,
             'price': Decimal('10.10'),
-            
         }
         res = self.client.post(RECIPES_URL, payload)
 
