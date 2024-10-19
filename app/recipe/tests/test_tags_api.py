79ac6b1b391c3a191c674f4c011a36647a4573b9
Esharat <esharat@esharat.bracu.ac.bd>
2024-10-19 13:34:56 +0600

    added tag serializer, viewset, router register and test case.

---
+++ app/recipe/tests/test_tags_api.py
@@ -0,0 +1,83 @@
+"""
+Tests for the tags API.
+"""
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import TestCase
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from core.models import Tag
+
+from recipe.serializers import TagSerializer
+
+
+TAG_URL = reverse('recipe:tag-list')
+
+
+def create_user(email="test@bracu.ac.bd", password="testpass3"):
+    """Create and return a new user."""
+    return get_user_model().objects.create_user(email=email, password=password)
+
+
+class PulicTagsApiTests(TestCase):
+    """"Test unauthenticated API requests."""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_auth_required(self):
+        """Test auth is required for retriving tags."""
+        res = self.client.get(TAG_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_create_without_auth(self):
+        """Test for createing a tags without auth is unsuccessful."""
+        user = create_user()
+        # Tag.objects.create(user=user, name="Fruti")
+        payload = {
+            'user': user,
+            'name': "Fruty",
+        }
+
+        res = self.client.post(TAG_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateTagsApiTests(TestCase):
+    """Test authenticated API requests."""
+
+    def setUp(self):
+        self.user = create_user()
+        self.client = APIClient()
+        self.client.force_authenticate(self.user)
+
+    def test_retrive_tags(self):
+        """Test retriving a list of tags."""
+        Tag.objects.create(user=self.user, name="Vegan")
+        Tag.objects.create(user=self.user, name="Dessert")
+
+        res = self.client.get(TAG_URL)
+
+        tags = Tag.objects.all().order_by('-name')
+        serializer = TagSerializer(tags, many=True)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_tags_limited_to_user(self):
+        """Test list of tags is limited to authenticated user."""
+
+        user2 = create_user(email="user2@bracu.ac.bd")
+        Tag.objects.create(user=user2, name="Fruity")
+        tag = Tag.objects.create(user=self.user, name="Juicy")
+
+        res = self.client.get(TAG_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), 1)
+        self.assertEqual(res.data[0]['name'], tag.name)
+        self.assertEqual(res.data[0]['id'], tag.id)
