18a8543922b3f0ad64951d63334c5b6326dd1636
Esharat <esharat@esharat.bracu.ac.bd>
2024-10-19 11:44:21 +0600

    added tag model class in core and write test case of models.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,12 @@
 """Tests for Models"""
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from core import models
+
+
+def create_user(email="test@bracu.ac.bd", password="testpass123"):
+    """Create and return a new user."""
+    return get_user_model().objects.create_user(email, password)
 
 
 class modelTests(TestCase):
@@ -44,3 +50,10 @@ class modelTests(TestCase):
             )
         self.assertTrue(super_user.is_superuser)
         self.assertTrue(super_user.is_staff)
+
+    def test_create_tag(self):
+        """Test creating a tag is successful."""
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name="Tag1")
+
+        self.assertEqual(str(tag), tag.name)
